c4d5ba6489f1356b1ebde51be596d99a9a59a3df
Stephan Dowless <dowless.stephan@gmail.com>
2020-11-14 18:06:02 -0500

    Initial Commit

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutorialApp.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Stephen Dowless on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
